b0ca1e8dd51a77f34c633c23e0002815bc34900f
Dmitriy Belkin <demetrius.belkin@gmail.com>
2021-07-28 22:07:29 +0200

    Надо пропустить получение токена и захаркодить

--- conftest.py
+++ conftest.py
@@ -1,6 +1,18 @@
 import requests
 
-
-def authorization():
-    r = requests.get('http://dev3.aliway.ru/cabinet/login/')
-    r.text
+""""Метод авторизации, записи токена в сессию"""
+url = 'http://dev3.aliway.ru/cabinet/login/'
+get = requests.get(url)
+cookies = get.cookies.values()
+a = requests.session()
+a.get(url)
+header = a.cookies
+se = header.get_dict()
+print(se)
+cred = {'username': 'admin@test.ru', 'password': 'admintestru'}
+csrfmiddlewaretoken = {'csrfmiddlewaretoken': cookies}
+payload = {**cred, **csrfmiddlewaretoken}
+headers = {'Cookie': cookies}
+post = requests.post(url, header=header, data=payload)
+print(payload)
+print(post.status_code)
--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 import requests
-import sys
-from collections import defaultdict
 
+url = "http://dev3.aliway.ru/cabinet/login/"
+response = requests.get(url)
+print(response.status_code)
+ra = response.cookies.values()
+print(ra)
+
+payload={'username': 'admin@test.ru','password': 'admintestru','csrfmiddlewaretoken': 'L1DHf1ahj2w6IX3kwAGPULUKZSeDL7zSAFReEzTX9VWuafO8xG5x8oIdmJBhFs64'}
+headers = {'Cookie': 'csrftoken=L1DHf1ahj2w6IX3kwAGPULUKZSeDL7zSAFReEzTX9VWuafO8xG5x8oIdmJBhFs64'}
+response = requests.request("POST", url, headers=headers, data=payload)
+print(response.status_code)
+
+r = requests.get(url)
+ar = r.cookies.values()
+print(ar)
+
+
+
+
+
+
+
+
+
+
+
+import requests
+
+""""Метод авторизации, записи токена в сессию"""
 url = 'http://dev3.aliway.ru/cabinet/login/'
-session = requests.session()
+session = requests.Session()
 cookies = session.get(url)
-headers = session
-print(headers)
+cred = {'username': 'admin@test.ru', 'password': 'admintestru'}
 csrftoken = cookies.cookies.get_dict()
-print(cookies.cookies.get_dict())
-cred = {'username' : 'admin@test.ru', 'password' : 'admintestru'}
-print(cred)
 payload = {**cred, **csrftoken}
+post = requests.post(url, headers=headers, data=payload)
+print(csrftoken)
 print(payload)
-post = requests.post(url, headers=cookies, json=payload)
 print(post.status_code)
