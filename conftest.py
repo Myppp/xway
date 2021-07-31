70da683e009228899e2c2c3c1ede2eb424ac8e87
Dmitriy Belkin <demetrius.belkin@gmail.com>
2021-07-31 22:43:25 +0200

    Авторизация работает корректно. Завел фикстуру для авторизации. Нужно сократить url до метода и сделать валидацию схемы

--- conftest.py
+++ conftest.py
@@ -1,18 +1,22 @@
+import pytest
 import requests
 
 
-""""Метод авторизации, получаем токен и sessionid"""
-url = 'http://dev3.aliway.ru/cabinet/login/'
-get_login = requests.get(url)
-cookies_full = get_login.cookies.values()
-cookies = "','".join(cookies_full)
-headerest = ['csrftoken=', cookies]
-delimiter = ''
-head = delimiter.join(headerest)
-header = {'Cookie': head}
-payload = {'username': 'admin@test.ru', 'password': 'admintestru', 'csrfmiddlewaretoken': cookies}
-session = requests.Session()
-post_login = session.post(url, headers=header, data=payload)
-session_data = post_login.request.headers
-dict_iter = [s.replace('python-requests/2.26.0', '') for s in session_data.values()]
-headers = {'Cookie': dict_iter[3-4]}
+@pytest.fixture(scope='session')
+def headers():
+    """"Метод авторизации, получаем токен и sessionid"""
+    url = 'http://dev3.aliway.ru/cabinet/login/'
+    get_login = requests.get(url)
+    cookies_full = get_login.cookies.values()
+    cookies = "','".join(cookies_full)
+    headerest = ['csrftoken=', cookies]
+    delimiter = ''
+    head = delimiter.join(headerest)
+    header = {'Cookie': head}
+    payload = {'username': 'admin@test.ru', 'password': 'admintestru', 'csrfmiddlewaretoken': cookies}
+    session = requests.Session()
+    post_login = session.post(url, headers=header, data=payload)
+    session_data = post_login.request.headers
+    dict_iter = [s.replace('python-requests/2.26.0', '') for s in session_data.values()]
+    headers = {'Cookie': dict_iter[3 - 4]}
+    return headers
--- tests/export/get-info-categories/test_get_info_categories.py
+++ tests/export/get-info-categories/test_get_info_categories.py
@@ -1,8 +1,7 @@
 import requests
 
 
-def test_get_info_categories():
+def test_get_info_categories(headers):
     """"Проверка GET запроса метода /export-system-settings/marketplace/{id}/get-info-categories/"""
-    headers = {  'Cookie': 'csrftoken=7CqbOzOuXSTAicNV7KkzLH4R3ZtbTrEZeFSOB1dt8CJa8KVu36Zd0pT2eOrPaogN; sessionid=ipmr718cornkqebcej60lxfpnwul8vqd'}
     result = requests.get(url='http://dev3.aliway.ru/export-system-settings/marketplace/1/get-info-categories/', headers=headers)
     assert result.status_code == 200
